fe0f779a73a71300458e6c8d21c7c8fa4c3fdf96
Derek Wheeler <derek.wheeler1f913@proton.me>
2025-10-16 15:50:47 -0600

    added a discription and photo to the about page

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,9 +1,26 @@
 function AboutUs() {
   
   return (
-    <>
-        <p>AboutUs Page</p>
-    </>
+    <div className="aboutUsBody">
+        <img className="profilePic" src="https://n19huqbctshx4yio.public.blob.vercel-storage.com/images/greetings/afternoon-V17aAtEqiGtoBZfVLl05tWfscHRh04.jpg"></img>
+        <p>Welcome to the SchwetzDeitch.org website. The website is a tool for learning and practicing the language Deitsch. Also called Dutch or Pennsylvania Dutch. This project is a labor of love. I am making it first and foremost to help me practice the language but I have made it public and freely available as a gift to the community as an available resource for anyone who may find it helpful.</p>
+        <br></br>
+        <p>My name is Derek and I am Fancy Dutch. I was raised outside of Pennsylvania but I was raised knowing my heritage. It's unknown when my family lost the language. My Dutch grandmother who was from Pennsylvania died when I was young, so I can't ask her anymore and I know my mother never knew the language.</p>
+        <br></br>
+        <p>For a long time I thought the language of the Fancy Dutch had completly dissappeared. I knew that our cousins the Plain Dutch still spoke a similar varity of the language but since they are an insular people I thought it was a lost cause that I would be able to learn the language. As an adult I learned that there are still Deitsch speakers in Pennsylvania and some resources online and in person in Pennsylvania to learn Deitsch. So I have begun the process of seriously learning Deitsch and attempting to reconnect with my culture and roots</p>
+        <br></br>
+        <p>I am so Incredibly fortunate that my wonderful husband is learning Deitsch with me and fully supportive of building a Fancy Dutch family with me. I am endlessly greatful to him and this would not be possible without his support.</p>
+        <br></br>
+        <p>As I was begging to learn Deitsch myself I started to make flashcard by hand to practice with. I was quickly struggling and frustrated however because I was struggling to remember how words were pronounced, and it was disruptive to have to put down my flash cards and search for videos of speakers.</p>
+        <br></br>
+        <p>I am a gradguate of a web development coding boot camp. So I realized I could fairly quickly make a website to store my flashcards and include audio and photos. The easiest way to make the website available for me to use wherever I am was to actually publish it; and that was the start of this project.</p>
+        <br></br>
+        <p>Since I was not raised Speaking Deitsch this project is prone to errors. I do as much research as I can and I use this website myself so as I learn more I will go back and fix mistakes. But please keep in mind that I am not an authority on Deitsch so take what you learn here with a grain of salt, use other learning resources, and doubt and question what's here.</p>
+        <br></br>
+        <p>I hope to eventually add many more features and fleshout this website as a complete learning resource. In the meantime I recomend the book "Schwetz mol Deitsch" as a helpful resource. I recomend the youtube channel by Douglas Madenford as well. He's amazing and I don't think I could learn the language myself if it wasn't for his many years of creating learning resources himself. Many of the audio clips used in this website are clipped from his videos.</p>
+        <br></br>
+        <p>I am an advocate of language diversity and language revitilization. I think the world is a richer and better place when we have many different cultures and languages. I encorage anyone who want's to to learn Deitsch. You are fully welcome to learn Deitsch for any reason. Whelther you have heritage in the culture or you're a pollyglot who find's the challenge fun. I also deeply care about the tragity of language extinction and I would encorage everyone to look into their roots because more than likely you have an ancestrial language that is dead or dieing and if you don't learn Deitsch then I would love to be the inspiration you need to revatalize other languages. Let's make the world a more interesting place.</p>
+    </div>
   )
  }
 
