6594b015ec59778fd3c29daea1a3b40d75281c81
Derek Wheeler <derek.wheeler1f913@proton.me>
2025-11-23 21:06:30 -0700

    cleaned up the about us page before publishing the update

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -4,8 +4,8 @@ function AboutUs() {
     <main className="aboutUsBody">
         <img className="profilePic" src="https://n19huqbctshx4yio.public.blob.vercel-storage.com/images/greetings/afternoon-V17aAtEqiGtoBZfVLl05tWfscHRh04.jpg"></img>
         <section className="english">
-          <p>Welcome to SchwetzDeitsch.org This website is a labor of love and a gift to my community. The goal is for this website to become a much needed resource for learning Dutch, ie Pennsyilvania Dutch, ie Deitsch.</p>
-          <p>This website is a companion tool that is best used together with as many resources you can get for learning Dutch. I will add as much to the website as I can but in general there are better explinations of grammer and definitions of words available in print and on the internet than you will find here.</p> 
+          <p>Welcome to SchwetzDeitsch.org This website is a labor of love, and a gift to my community. The goal of this site is to become a much needed resource for learning Dutch, ie Pennsylvania Dutch, ie Deitsch.</p>
+          <p>This website is a companion tool that is best used in conjunction with as many resources you can get for learning Dutch. I will add as much to the website as I can but in general there are better explinations of grammar and definitions available in print and on the internet than you will find here.</p> 
           <p>This site will be most helpful for practicing, refershing, expanding, and solidifying the concepts you are also learning elsewhere.</p>
         </section>
         <h3 className="english">Recomended learning recourses</h3>
@@ -63,8 +63,9 @@ function AboutUs() {
           <p>
             To start you can click on the practice button in the navigation bar.
             There you will find a list of flashcard decks that I have made.
-            You can click on multiple decks you want to practice and then click start.
-            You will then be presented with a prompt flash card.
+            When you click on a deck it will highlight.
+            Click on the decks that you want to practice and click start.
+            You will then be presented with a prompt side of a flash card.
             When you are ready you can click flip and you will see the answer side of the flash card.
             You can then score yourself and a new card will appear.
             Behind the scenes the score increases or decreases the rate that the same card will reappear.
@@ -72,25 +73,26 @@ function AboutUs() {
             You can restart and pick new decks anytime you want, and come back for quick practices with no time commitment.
           </p>
           <p>
-            Different cards may have a different format.
-            Some may have you translate a phrase from english to Dutch.
-            While others may have you respond to a question in Dutch.
-            But most importantly I am not the boss of you.
-            The cards exist and you can use them however makes sense to you and howwever you find helpful.
+            Some cards have different formats.
+            The basic card has English on the front and Dutch on the back.
+            Cards may also have images and audio to aid in learning.
+            I often include a text box for you to write your response, and I encorage you to speak your response aswell.
+            Please keep in mind that these decks are a tool that you can use however you find helpful.
             Some cards will have a text box for you to write down your answer, to help with writting and to better compare with the answer.
-            But you don't have to use it, respond outloud or in your head if you want.
+            But you don't have to use the textbox, respond outloud or in your head if you want.
+            My goal is to give you tools that you can use, feel free to ignore any element that isn't working for you.
           </p>
           <p>
             A unique part of this language learning system is the self grading. 
             You wont be judged here, by a robot or a teacher.
-            The idea is to remove embarcement and barriors to practice and putting yourself out there.
+            The idea is to remove embarrassment and barriers to practice and putting yourself out there.
             After a card is finished you can decide for youself if you need to see that card more friquently or less friquently.
             You also get to decide if you remebered correctly or pronounced close enough etc.
-            Also in language there is rarely if ever 1 correct answer.
+            Also in language learning there is rarely if ever only one correct answer.
             Some cards may ask you a question.
             The answer side of the card will show you the answer I intended. 
-            I have attempted to verify the grammer for the Dutch on the site.
-            But if you feel your answer was correct then it is!
+            I have attempted to verify the grammar for the Dutch on the site.
+            But if you feel your answer was different but also correct then give yourself a green check and keep going!
             </p>
             <p>
               As I build the website I plan on writting companion lessons that you can review as needed.
@@ -99,68 +101,26 @@ function AboutUs() {
               Instead the dictionary is intended to be a resource for only the words that actually appear in the site.
               Ideally cards will have links to pages in the dictionary section of the site.
               That way you can read more explinations of the grammer or translations or definitions of a word or phrase.
-              In general I try to avoid translation in language learning.
-              So by having the translations and definitions availbe but out of sight, you can learn all about a word but then when practicing you can rely on english less and practice more monolingually.
             </p>
         </section>
         <h3>About Us</h3>
         <section>
           <p>
             This website is made only by me. 
-            Hi, I'm Derek. I am a computer programmer, and I am Fancy Dutch. 
-            A common story for many of the Fancy Dutch is that in the late 1800s or early 1900s the language was not passed down; 
-            during periouds of intense ethnic and linguistic based abuse in the united states.
-            I am working to revive the language in my family, learn the language myself, and help revitalize the Dutch language and culture.
+            Guder Daag! I'm Derek. I am a computer programmer, and I'm Fancy Dutch. 
+            A common story for many of the Fancy Dutch is that in the late 1800s or early 1900s the language was not passed down to the next generation; 
+            during periods of intense ethnic and linguistic based abuse in the united states.
+            That is the case for my family.
           </p>
           <p>
-            This website is actually primarily for myself, my husband, and my family.
-            Publishing the website was actually easier for me to create a language learning tool for us that I can share and we can access wherever we are.
-            As such we are also the test subjects.
+            I created this website primarily for myself, my husband, and my family.
+            Publishing the website was actually the easiest way for me to create a portable language learning tool for us.
+            Because I am also using this website I am also the text subject.
             I am going to learn Dutch.
             I am very passinate about it.
-            And I am collect what I learn here.
-            If a stradegy is working I'll add more of it, and if it is failing then I will switch gears to other strategies.
+            And I am going to collect what I learn here.
+            If a strategy is working I'll add more of it, and if it is failing then I will switch gears to other strategies.
             Also my husband has a very differen't learning style than me so by making the website work well for both of us then maybe it will work well for you.
-          </p>
-        </section>
-        <h3>Copy Right and Legal</h3>
-        <section>
-          <p>
-            I have taken care to follow copyright law in the creation of this website.
-            Images on the site are nearly all photos taken by me or art made by me. The text, definitions, and translations on this site are written by me.
-            The audio on the site is short snippits of audio that I have taken from longer sources.
-            I cut out individual words or sentences to use as examples of pronunciation.
-            I edit and curate the clips myself.
-            It is my claim that this use is fair use legally.
-          </p>
-          <p>
-            Copy right protections in The United States exist automatically, the instant that a creative work is created.
-            For a website that applies to the content and the source code.
-            It's definitly weird that congress considers software the same as creative writting but I didn't write the law.
-            The source code for this website is availbe to be viewed publicly. This does not forfit my rights. 
-            The purpose, of having the source code publicly viewable is for portfolio purposes and security. 
-            You are welcome to review the code for evaluating my skills as a programmer, or evaluating the security and saftey of the website.
-          </p>
-          <p>
-            Privacy: I host this website via the web hosting platform Vercel.
-            The provide an analytics tool that I make use of.
-            You can read up on the vercel Analytics tool
-            <a href="https://vercel.com/docs/analytics"> here </a>
-            to learn how it works.
-            Mostly it tells me things like how many people view the site, how many people are online, and the country location of your IP adress.
-            It's helpful to know roughly how many and when people use the site, and rough locations of people.
-            That way if I start getting lots of visitors from a random country I can notice I am being scammed or hacked.
-            I pay for this site myself and don't generate revinue from it.
-            As long as it's being used approprately that's fine because there just aren't that many of us.
-            Other than Vercel Analitics, this site doesn't collect or store information. You can't even make an account.
-            Really the only things that I can learn is the information your browser is already telling every site you visit anyway.
-            If you don't wan't that information out there, if you use privacy focused browser and/or a vpn that should help.  
-          </p>
-          <p>
-            Finally I would ask that if you think this website has infringed on your copyright.
-            I would kindly ask that you let it go.
-            The Fancy Dutch Language is critically endangered.
-            Preserving and revitilizing this language is important work, and we are short handed.
           </p>
         </section>
         <h3>Contact Us</h3>
